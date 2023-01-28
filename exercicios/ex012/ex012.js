a7f4d63cebf22451467d32a4c202e00afc4fe7d8
DavidSilva32 <david.silvaxd32@gmail.com>
2023-01-27 23:27:35 -0300

    Update ex012.js

--- exercicios/ex012/ex012.js
+++ exercicios/ex012/ex012.js
@@ -1,10 +1,9 @@
 var agora = new Date()
 var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
+console.log(`Agora são exatamente ${hora} hora.`)
 if (hora < 6) {
     console.log('Boa Madrugada!')
-}    
-else if (hora < 12) {
+} else if (hora < 12) {
     console.log('Bom Dia!')
 } else if (hora < 18) {
     console.log('Boa Tarde!')
