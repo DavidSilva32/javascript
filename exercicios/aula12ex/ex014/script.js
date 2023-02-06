126a13e19c794784285bb34efba3f2d11722d3d3
DavidSilva32 <david.silvaxd32@gmail.com>
2023-02-06 11:39:37 -0300

    Update script.js

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -7,21 +7,21 @@ function carregar() {
 
     if (hora >= 6 && hora <= 12) {
         //BOM DIA
-        msg.innerHTML = `Agora são ${hora}:${min} da Manhã`
+        msg.innerHTML = `Agora são <STRONG>${hora}:${min}</STRONG> da Manhã`
         document.body.style.background = '#CEBB87'
     } else if (hora > 12 && hora <= 18 ) {
         //BOA TARDE
-        msg.innerHTML = `Agora são ${hora}:${min} da Tarde`
+        msg.innerHTML = `Agora são <STRONG>${hora}:${min}</STRONG> da Tarde`
         img.src = 'imagens/foto-tarde.jpg'
         document.body.style.background = '#B17351'
     } else if (hora > 18 && hora < 24) {
         //BOA NOITE
-        msg.innerHTML = `Agora são ${hora}:${min} da Noite`
+        msg.innerHTML = `Agora são <STRONG>${hora}:${min}</STRONG> da Noite`
         img.src = 'imagens/foto-noite.jpg'
         document.body.style.background = '#081C1D'
     } else {
         //BOA MADRUGADA
-        msg.innerHTML = `Agora são ${hora}:${min} da Madrugada`
+        msg.innerHTML = `Agora são <STRONG>${hora}:${min}</STRONG> da Madrugada`
         img.src = 'imagens/foto-madrugada.jpg'
         document.body.style.background = '#554266'
     }
