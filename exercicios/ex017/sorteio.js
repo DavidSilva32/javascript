5f182fa521d630e6ecad48d5aed25aa6a8777276
DavidSilva32 <david.silvaxd32@gmail.com>
2023-03-28 06:48:30 -0300

    created ex017

---
+++ exercicios/ex017/sorteio.js
@@ -0,0 +1,5 @@
+// Global Variables
+let button = []
+button[0] = document.getEleentsByTagName('button')[0]
+button[1] = document.getEleentsByTagName('button')[1]
+let res = document.querySelector('section#resultado')
